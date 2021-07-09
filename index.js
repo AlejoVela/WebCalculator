e4170be2b8ef184d697da3d4257da69f40830726
Diego R <alejorvela@gmail.com>
2021-07-09 09:52:32 -0500

    Front Ready! (first commit)

---
+++ index.js
@@ -0,0 +1,29 @@
+let $btn0 = document.getElementById("btn0");
+let $btn1 = document.getElementById("btn1");
+let $btn2 = document.getElementById("btn2");
+let $btn3 = document.getElementById("btn3");
+let $btn4 = document.getElementById("btn4");
+let $btn5 = document.getElementById("btn5");
+let $btn6 = document.getElementById("btn6");
+let $btn7 = document.getElementById("btn7");
+let $btn8 = document.getElementById("btn8");
+let $btn9 = document.getElementById("btn9");
+let $btnD = document.getElementById("btnD");
+let $btnCE = document.getElementById("btnCE");
+let $btnDiv = document.getElementById("btnDiv");
+let $btnDel = document.getElementById("btnDel");
+let $btnX = document.getElementById("btnX");
+let $btnSQ = document.getElementById("btnSQ");
+let $btnM = document.getElementById("btnM");
+let $btnPi = document.getElementById("btnPIi");
+let $btnPlus = document.getElementById("btnPlus");
+let $btnEq = document.getElementById("btnEq");
+let $pantalla = document.getElementById("pantalla");
+
+
+
+
+
+
+
+
