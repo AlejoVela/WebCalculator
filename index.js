dda8cceadd7ddc233014fee86577a47e07d0f054
Diego R <alejorvela@gmail.com>
2021-07-09 10:15:02 -0500

    numbers ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Variables dom
 let $btn0 = document.getElementById("btn0");
 let $btn1 = document.getElementById("btn1");
 let $btn2 = document.getElementById("btn2");
@@ -19,6 +20,68 @@ let $btnPi = document.getElementById("btnPIi");
 let $btnPlus = document.getElementById("btnPlus");
 let $btnEq = document.getElementById("btnEq");
 let $pantalla = document.getElementById("pantalla");
+//variables globales
+let pi = 3.141592;
+let resultado = 0;
+let acumulator = 0;
+
+//funciones
+const getData = (data) => {
+    pantalla.innerHTML += data.innerHTML;
+};
+const cleanDisplay = () => {
+    pantalla.innerHTML = "";
+};
+const oparation = (op) => {
+    
+};
+
+
+
+//eventos
+$btn0.onclick = function () {
+    getData($btn0);
+};
+$btn1.onclick = function () {
+    getData($btn1);
+};
+$btn2.onclick = function () {
+    getData($btn2);
+};
+$btn3.onclick = function () {
+    getData($btn3);
+};
+$btn4.onclick = function () {
+    getData($btn4);
+};
+$btn5.onclick = function () {
+    getData($btn5);
+};
+$btn6.onclick = function () {
+    getData($btn6);
+};
+$btn7.onclick = function () {
+    getData($btn7);
+};
+$btn8.onclick = function () {
+    getData($btn8);
+};
+$btn9.onclick = function () {
+    getData($btn9);
+};
+$btnD.onclick = function () {
+    getData($btnD);
+};
+$btnCE.onclick = function () {
+    cleanDisplay();
+};
+/*
+$btn.onclick = function () {
+    getData($btn);
+};*/
+
+
+
 
 
 
